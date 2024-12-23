58a0b69fa30860ebf3ef73522189ea4259da33c4
marrowbuster <marrowbuster@gmail.com>
2024-12-22 18:33:38 -0800

    update to ItemDisplay Tutorial specs on PaperMC, but I can already tell this code is probably gonna lag the server because I'm creating a new runTaskTimer for each.

--- src/main/java/com/marrowbuster/learningPaperMC/LearningPaperMC.java
+++ src/main/java/com/marrowbuster/learningPaperMC/LearningPaperMC.java
@@ -15,10 +15,17 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.joml.Matrix4f;
 
 public class LearningPaperMC extends JavaPlugin implements Listener {
 
+    private final Plugin plugin;
+
+    public LearningPaperMC(Plugin plugin) {
+        this.plugin = plugin;
+    }
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
@@ -47,14 +54,21 @@ public class LearningPaperMC extends JavaPlugin implements Listener {
                 event.getPlayer().sendMessage(Component.text(event.getPlayer().getName() + " threw a sword into the sky."));
             }
             Location playerLocation = p.getLocation();
-            playerLocation.getWorld().spawn(playerLocation.clone(), ArmorStand.class, (spawnedStand) -> {
-                spawnedStand.setVisible(false);
-                spawnedStand.setSmall(true);
-                spawnedStand.setItem(EquipmentSlot.HAND, new ItemStack(Material.IRON_SWORD));
-                event.getPlayer().sendMessage(Component.text("original velocity: " + spawnedStand.getVelocity().length()));
-                spawnedStand.setVelocity(p.getLocation().getDirection().multiply(3));
-                event.getPlayer().sendMessage(Component.text("new velocity: " + spawnedStand.getVelocity().length()));
+            ItemDisplay swordDisplay = playerLocation.getWorld().spawn(playerLocation.clone(), ItemDisplay.class, (disp) -> {
+                disp.setRotation(-180f + 1 * 120f, 0);
+                disp.setItemStack(new ItemStack(Material.GOLDEN_SWORD));
             });
+            Matrix4f mat = new Matrix4f().scale(0.5F);
+            Bukkit.getScheduler().runTaskTimer(plugin, task -> {
+                if (!swordDisplay.isValid()) { // display was removed from the world, abort task
+                    task.cancel();
+                    return;
+                }
+
+                swordDisplay.setTransformationMatrix(mat.rotateY(((float) Math.toRadians(180)) + 0.1F /* prevent the client from interpolating in reverse */));
+                swordDisplay.setInterpolationDelay(0); // no delay to the interpolation
+                swordDisplay.setInterpolationDuration(1); // set the duration of the interpolated rotation
+            }, 1 /* delay the initial transformation by one tick from display creation */, 1);
         }
     }
 
